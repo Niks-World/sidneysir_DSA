825484a315e3d4076d54921387df85ed711b3651
Niks-World <nikkalyan52@gmail.com>
2023-07-16 03:44:34 +0530

    nasted

---
+++ forloop.java
@@ -0,0 +1,14 @@
+public class forloop {
+    public static void main(String[] args) {
+            for(int count=1; count<= 10; count++){ //Q1 print hello 10 time
+                System.out.println("Hello");
+    }
+
+        for(int i=2; i<=100; i=i*2){ // print power of 2 <= 100 (2*2=4*4=8*8=16*16=32*32=64*64=128 stop 100 se bada hai)
+            System.out.println(i);
+        }
+
+
+    }
+
+}
