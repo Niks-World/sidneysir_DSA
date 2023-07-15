7a89df46b9b3004a2e5ced2e6eae260f35940bcc
Niks-World <nikkalyan52@gmail.com>
2023-07-16 03:43:22 +0530

    nasted

---
+++ nastedloop.java
@@ -0,0 +1,24 @@
+
+// public static ""void"" main(String[] args) 
+//     for( int r=1; r<=10;r++)
+//     for(int m=1; m<=5; m++)
+//         system.out.println("GG");
+    
+//     system.out.println();
+
+
+public class nastedloop {
+    public static void main(String[] args) {
+        for( int r=1; r<=10;r++){
+    for(int m=1; m<=5; m++){
+         System.out.println("GG");
+
+        };
+        System.out.println("GG");
+
+        };
+
+    }
+}
+//op=> GG GG GG GG 10 TIME
+
