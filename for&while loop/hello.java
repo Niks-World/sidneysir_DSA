7a6b92e63fcf6fbdbd5ab68011892aec2f97f275
Niks-World <nikkalyan52@gmail.com>
2023-07-18 06:17:09 +0530

    forinfor

---
+++ for&while loop/hello.java
@@ -0,0 +1,25 @@
+public class hello {
+    public static void main(String[] args) {
+        // print hello 10 times
+        System.out.println("Hello Nik"); //Hello Nik
+        System.out.println("Hello Nik"); //Hello Nik
+        System.out.println("Hello Nik");
+        System.out.println("Hello Nik");       
+        System.out.println("Hello Nik");        
+        System.out.println("Hello Nik");       
+        System.out.println("Hello Nik");
+// Print using FOR loop
+        for(int i=1; i<=10;i++){
+          System.out.println("Hello Nik");
+        }
+// op => Hllo NIk is 10 time
+System.out.println("---------");
+// print using while loop
+        int count =1;
+        while(count <=10){
+            System.out.println("Hello Nik");
+            count =count +1;
+        }
+
+    }
+}
---
+++ num.java
@@ -0,0 +1,17 @@
+public class num {
+    public static void main(String[] args) {
+          int num1 = 4;
+          int num2 =8;
+          int num3=10;
+//printing the true or false num is big than num 2 no so false 
+          System.out.println(num1 > num2);
+          num1 = num1 +num3;
+          System.out.println(num1 > num2);
+          
+
+
+
+
+
+    }
+}
