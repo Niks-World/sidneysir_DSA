c0c327442a85f3f8e024213d69c143325b1b204d
Niks-World <nikkalyan52@gmail.com>
2023-07-21 06:41:11 +0530

    multiDimsArray

--- MultiDimensionArr/twoD3Darr.java
+++ MultiDimensionArr/twoD3Darr.java
@@ -3,7 +3,8 @@ package MultiDimensionArr;
 public class twoD3Darr{
 public static void main(String[] args) {
 
-   // print the 2 ,5 from given element 
+   // print the 2 ,5 from given element
+   
     int [][] A = new int[][]{
     {12,8,5,7},
     {3,1,13,2},
