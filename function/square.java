0ce7b8064e91f68de8039f31ae222eaef4e8fdd5
Niks-World <nikkalyan52@gmail.com>
2023-07-24 04:26:29 +0530

    function done

---
+++ function/square.java
@@ -0,0 +1,27 @@
+package function;
+
+public class square {
+    
+        public static int square(int x){
+          return x*x;
+        }
+      
+      // public static String Welcome(String name){
+      //   return "Hi " +name+ "! Wwlcome to Masai.";
+      // }
+        
+        public static void main(String[] arges){
+      
+        int a = 3, b = 4;
+      
+          int sq1 = square(a);
+          int sq2 = square(b);
+      
+          System.out.print(sq1 + sq2);
+      
+          // System.out.println(Welcome("NIKHIl"));
+      
+          
+        }
+      }
+
