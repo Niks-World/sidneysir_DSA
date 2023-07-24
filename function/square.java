4a9cbc610ecb3686813ba735076083839dfa2e5e
Niks-World <nikkalyan52@gmail.com>
2023-07-25 04:14:06 +0530

    square er fix

--- function/square.java
+++ function/square.java
@@ -1,30 +1,19 @@
 package function;
 
 public class square {
-    public static void main(String[] agrs){
-        public static int square(int x){
-          return x*x;
-        }
-      
-      // public static String Welcome(String name){
-      //   return "Hi " +name+ "! Wwlcome to Masai.";
-      // }
-        
-        public static void main(String[] arges){
-      
-        int a = 3, b = 4;
-      
-          int sq1 = square(a);
-          int sq2 = square(b);
-      
-          System.out.print(sq1 + sq2);
-      
-          // System.out.println(Welcome("NIKHIl"));
-      
-        
-        }
+
+    // Method to calculate the square of a number
+    public static int square(int x) {
+        return x * x;
     }
 
-  }
+    // Main method to test the square function
+    public static void main(String[] args) {
+        int a = 3, b = 4;
 
-    // Some out is not coming 
+        int sq1 = square(a);
+        int sq2 = square(b);
+
+        System.out.print(sq1 + sq2);
+    }
+}
