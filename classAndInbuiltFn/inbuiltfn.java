e459d3d886e86be1718ad844c4b1ee3758dda90e
Niks-World <nikkalyan52@gmail.com>
2023-07-28 03:03:31 +0530

    inbuilt function

--- classAndInbuiltFn/inbuiltfn.java
+++ classAndInbuiltFn/inbuiltfn.java
@@ -4,7 +4,7 @@ class inbuiltfn {
 
 public static void main(String[] args) {
   
-    // 27 /07/23 complted only left to practice on vs code
+    // 27 /07/23 complted only left to practice on vs code please do as soon as posible
 
 
 
