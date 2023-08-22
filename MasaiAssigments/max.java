a4726a6029f50f2d24e3dffaee9756c07a0b2b4f
Niks-World <nikkalyan52@gmail.com>
2023-08-23 02:52:16 +0530

    substring

--- MasaiAssigments/max.java
+++ MasaiAssigments/max.java
@@ -2,6 +2,26 @@ package MasaiAssigments;
 
 public class max {
     public static void main(String[] args) {
+
+         String s = "masaiisverygoodinsitute";
+    int n = s.length(); // Get the length of the string
+    int count=0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j <n; j++) { 
+          for(int k=i;k<=j;k++){
+            System.out.print(s.charAt(k));
+            
+          }
+          System.out.println();
+          
+            // System.out.println(s.substring(i, j));
+         count++;
+        }
+         
+    }
+    System.out.println(count);
+}
         
     }
-}
+
