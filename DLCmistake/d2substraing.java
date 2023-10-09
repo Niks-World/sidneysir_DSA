9eaf370aa0440afa0b52ecee827f8d15368d270f
Niks-World <nikkalyan52@gmail.com>
2023-10-10 03:15:08 +0530

    subString dlc test ques solve

--- DLCmistake/d2substraing.java
+++ DLCmistake/d2substraing.java
@@ -3,6 +3,8 @@ package DLCmistake;
 
 import java.util.Arrays;
 
+import string.string;
+
 // import String.string;
 public class d2substraing {
     public static void main(String[] args) {
@@ -38,5 +40,31 @@ public class d2substraing {
         }
         System.out.println(Arrays.toString(destination));
         // output => [2, 4, 6, 8, 0]
+
+        //--------------------------------------------------
+
+        String text = "abababab";
+        String subString = "aba" ;
+        int count = 0;
+
+        for(int i = 0; i <= text.length() - subString.length(); i++){
+            if(text.substring(i, i + subString.length()).equals(subString)){
+                  count++;
+            }
+          
+        }
+        System.out.println(count);
+        // output => 3
+
+        //-----------------------------------------------------------
+
+        String[] names = {"Alice", "Bob", "Charlie" , "David"};
+        String concatenated ="";
+
+        for(int i = 0; i < names.length; i++){
+            concatenated += names[i] + " ";
+        }
+        System.out.print(concatenated);
+        // output => 3 its conting the spcae between nams
     }
 }
